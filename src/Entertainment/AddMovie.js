7e4d3059f183053d966186eabc1ade978c1b7c64
Biswa <barikbiswa143@gmail.com>
2023-03-31 15:19:41 +0530

    Making apps error proof

--- src/Entertainment/AddMovie.js
+++ src/Entertainment/AddMovie.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import MovieList from "./MovieList";
 import { Button } from "react-bootstrap";
@@ -6,21 +6,83 @@ import { Button } from "react-bootstrap";
 const MoviePage = () => {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [retry, setRetry] = useState(false);
+
   const fetchMovieHandler = async () => {
     setIsLoading(true);
-    const response = await fetch("https://swapi.dev/api/films/");
-    const data = await response.json();
-    const transformedMovies = data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date,
-      };
-    });
-    setMovies(transformedMovies);
+    setError(null);
+    try {
+      const response = await fetch("https://swapi.dev/api/film/");
+      if (!response.ok) {
+        throw new Error("Something went wrong....Retrying");
+      }
+      const data = await response.json();
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
     setIsLoading(false);
   };
+
+  let content = (
+    <p style={{ marginLeft: "39.8rem", marginTop: "0.5rem" }}>
+      No Movies Found
+    </p>
+  );
+
+  if (movies.length > 0) {
+    content = <MovieList movies={movies} />;
+  }
+
+  const cancelRetrying = () => {
+    content = <p></p>;
+    setError(null);
+    setRetry(false);
+  };
+
+  if (error) {
+    content = (
+      <div>
+        {" "}
+        {`${error}`}{" "}
+        <Button variant="outline-danger" onClick={cancelRetrying}>
+          {" "}
+          cancel
+        </Button>{" "}
+      </div>
+    );
+  }
+
+  useEffect(() => {
+    if(retry) {
+      const timer = setTimeout(() => {
+        fetchMovieHandler(setMovies, setIsLoading, setError , setRetry)
+        .setRetry(false); 
+        console.log('retrying..')
+      }, 5000);
+      return ()=>clearTimeout(timer);
+    }
+  },[retry,setMovies])
+
+  useEffect(()=>{
+    fetchMovieHandler(setMovies, setIsLoading, setError , setRetry) 
+  },[setMovies])
+
+  if (isLoading) {
+    content = (
+      <p style={{ marginLeft: "41.5rem", marginTop: "1rem" }}>Loading...</p>
+    );
+  }
+
   return (
     <>
       <section>
@@ -31,11 +93,7 @@ const MoviePage = () => {
           Fetch Movies
         </Button>
       </section>
-      <section>
-        {!isLoading && movies.length > 0 && <MovieList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p style={{marginLeft:'39.8rem', marginTop: "0.5rem"}}>No Movies Found</p>}
-        {isLoading && <p style={{marginLeft:'41.5rem', marginTop: "1rem"}}>Loading...</p>}
-      </section>
+      <section>{content}</section>
     </>
   );
 };
